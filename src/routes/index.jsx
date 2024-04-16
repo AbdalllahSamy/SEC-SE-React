e4fa2a4369956897b35c8fda5fcd953061609f95
abdallah <abdosamu538@gmail.com>
2024-04-17 01:21:33 +0200

    First Commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import AppLayout from "../components/layout/AppLayout";
+import LoginPage from "../pages/LoginPage";
+import MainLayout from "../components/layout/MainLayout";
+import DashboardPage from "../pages/DashboardPage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />
+      },
+      {
+        path: "dashboard-admin",
+        element: <MainLayout />,
+        children: [
+          {
+            index: true,
+            element: <DashboardPage />
+          } 
+        ]
+      },
+      {
+        path: "dashboard-sec",
+        element: <MainLayout />,
+        children: [
+          {
+            index: true,
+            element: <DashboardPage />
+          } 
+        ]
+      }
+    ]
+  }
+]);
